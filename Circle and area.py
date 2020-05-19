367ccac4197c32110c7b4ff1941dc68d948ed74e
Sourasish Das <43234998+Scorpiussmith13@users.noreply.github.com>
2020-05-20 01:52:59 +0530

    Add files via upload

---
+++ Circle and area.py
@@ -0,0 +1,8 @@
+#Python for github problem 2
+def circle(radius,pi=3.14):
+     return pi*(radius**2)
+ 
+radius=int(input("Enter the radius of the circle"))
+ans=circle(radius)
+print(" The area of the circle is ",ans)
+    
